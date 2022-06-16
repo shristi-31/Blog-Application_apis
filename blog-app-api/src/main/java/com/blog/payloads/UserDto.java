e29256dd754b8888a3867c0ee81f379a2cd1ebe7
shristi-31 <patidarshristi@gmail.com>
2022-06-16 19:51:10 +0530

    -

--- blog-app-api/src/main/java/com/blog/payloads/UserDto.java
+++ blog-app-api/src/main/java/com/blog/payloads/UserDto.java
@@ -1,22 +1,37 @@
 package com.blog.payloads;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
 public class UserDto {
+	
 	private int id;
+	@NotEmpty
+	@Size(min=4,message="username should be minm of 4 chars")
 	private String name;
+	@Email(message="entera valid email")
 	private String email;
+	@NotEmpty
+	@Size(min=5,max=15,message="should be only btw 5 n 15 ")
 	private String password;
+	@NotNull
 	private String about;
+	
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
--- blog-app-api/src/main/java/com/blog/services/impl/UserServiceImpl.java
+++ blog-app-api/src/main/java/com/blog/services/impl/UserServiceImpl.java
@@ -1,14 +1,18 @@
 package com.blog.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.blog.entities.User;
+import com.blog.exceptions.ResourceNotFoundException;
 import com.blog.payloads.UserDto;
 import com.blog.repositories.UserRepo;
 import com.blog.services.UserService;
 
+@Service
 public class UserServiceImpl implements UserService{
 
 	@Autowired
@@ -22,26 +26,37 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
-	public UserDto updateUser(UserDto user, Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDto updateUser(UserDto userDto, Integer userId) {
+		User user = this.userRepo.findById(userId).orElseThrow(()->new ResourceNotFoundException("User"," id ",userId));
+		user.setAbout(userDto.getAbout());
+		user.setEmail(userDto.getEmail());
+		user.setName(userDto.getName());
+		user.setPassword(userDto.getPassword());
+		
+		User updatedUser = this.userRepo.save(user);
+		return UserToDto(updatedUser);
 	}
 
 	@Override
-	public UserDto getUserById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDto getUserById(Integer userId) {
+		User user = this.userRepo.findById(userId).orElseThrow(()->new ResourceNotFoundException("User"," id ",userId));
+		return UserToDto(user);
 	}
 
 	@Override
 	public List<UserDto> getAllUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> listUsers = this.userRepo.findAll();
+		List<UserDto> list = new ArrayList<>();
+		for(User user:listUsers) {
+			UserDto uDto = UserToDto(user);
+			list.add(uDto);
+		}
+		return list;
 	}
 
 	@Override
 	public void deleteUser(Integer id) {
-		// TODO Auto-generated method stub
+		this.userRepo.deleteById(id);
 		
 	}
 	
